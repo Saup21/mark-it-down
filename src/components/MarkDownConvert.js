e277f395f6e1c5740dc119a14bea403488d2d0c5
Saup21 <sauparna.gupta@gmail.com>
2021-08-16 04:23:09 +0530

    things work and sessional storage added

--- src/components/MarkDownConvert.js
+++ src/components/MarkDownConvert.js
@@ -1,23 +1,48 @@
 import React, { useState, useEffect } from 'react'
 import MonacoEditor from '@monaco-editor/react'
+import ReactMarkdown from 'react-markdown'
 
 function MarkDownConvert() {
     const [mark, setMark] = useState('')
 
-    const options = {
-        selectOnLineNumbers: true,
-        fontSize: 16
-    }
+    useEffect(() => {
+        if (sessionStorage.getItem('code')) {
+            setMark(sessionStorage.getItem('code'))
+        } else {
+            sessionStorage.setItem('code', mark)
+        }
+    }, [])
+
+    useEffect(() => {
+        sessionStorage.setItem('code', mark)
+        console.log('storing');
+    }, [mark])
+
+
 
     return (
-        <div className="container">
+        <div className="container area">
             <div className="editor">
                 <MonacoEditor
                     language="markdown"
                     theme="vs-dark"
-                    options={options}
+                    options={{
+                        minimap: {
+                            enabled: true,
+                        },
+                        selectOnLineNumbers: true,
+                        fontSize: 16,
+                        wordWrap: "on"
+                    }}
+                    value={mark}
+                    onChange={(value, event) => { setMark(value) }}
                 />
-                
+            </div>
+            <div className="preview">
+                <div className="header-text">Your Preview appears here</div>
+                <ReactMarkdown>
+                    {mark}
+                </ReactMarkdown>
             </div>
         </div>
     )
