19b8af748c6a2c5be1346787e8e0260350e18b41
Saup21 <sauparna.gupta@gmail.com>
2021-08-18 02:17:11 +0530

    download option added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
 function Header() {
+
+    const download = e => {
+        e.preventDefault()
+        let text = sessionStorage.getItem('code')
+        let blob = new Blob([text], {
+            type: 'text/plain'
+        })
+        let anchor = document.createElement('a')
+        anchor.download = 'markdown_file.md'
+        anchor.href = window.URL.createObjectURL(blob)
+        anchor.target = '_blank'
+        anchor.style.display = 'none'
+        document.body.appendChild(anchor)
+        anchor.click()
+        document.body.removeChild(anchor)
+    }
+
     return (
-        <div>
-            <h2 className="text-center"><u>MARK-IT-DOWN</u></h2>
+        <div className="container">
+            <ul className="header">
+                <li className="left"><h2 className="text-left">MARK-IT-DOWN</h2></li>
+                <li className="right"><button type="button" className="btn btn-info btn-md" onClick={e => download(e)} id="save">Save</button></li>
+            </ul>
         </div>
     )
 }
